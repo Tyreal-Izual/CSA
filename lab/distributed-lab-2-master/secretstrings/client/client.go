8895575afd72270d6b57f1ebf1b32af86496cce3
Tyreal-Izual <Zouzhicheng1348099766@outlook.com>
2022-10-14 16:55:19 +0100

    1

---
+++ lab/distributed-lab-2-master/secretstrings/client/client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	//	"net/rpc"
+	"flag"
+	//	"bufio"
+	//	"os"
+	//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"fmt"
+)
+
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
+	flag.Parse()
+	fmt.Println("Server: ", *server)
+	//TODO: connect to the RPC server and send the request(s)
+}
